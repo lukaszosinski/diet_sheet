1dae30ab113fa4ef819242b6aa8b36f551113a7e
lukaszosinski <nuras9988@gmail.com>
2019-03-26 16:16:20 +0100

    XML config changed to java config.

---
+++ src/main/java/com/dietsheet/configuration/AppConfiguration.java
@@ -0,0 +1,15 @@
+package com.dietsheet.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+
+@Configuration
+@EnableWebMvc
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@ComponentScan(basePackages = "com.dietsheet")
+public class AppConfiguration {
+}
